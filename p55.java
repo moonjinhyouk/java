43b65ea23ad7439cec48a02be439882a94463179
moonjinhyouk <100898147+moonjinhyouk@users.noreply.github.com>
2024-09-14 17:54:29 +0900

    Add files via upload

---
+++ p55.java
@@ -0,0 +1,10 @@
+package p55;
+
+public class p55 {
+ public static void main(String [] args) {
+	 System.out.println("자바는 처음이죠?");
+	 System.out.println("Welcome to Java World~~");
+	 System.out.println("학기 시작되었어요!");
+	 System.out.println("이번 학기 자바를 열심히 공부해 최고의 Software 개발자가 될거에요~~");
+ }
+}
